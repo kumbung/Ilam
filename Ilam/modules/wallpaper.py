83728890e42247d8603a11cacb6342dc355c17b9
kumbung <48485818+kumbung@users.noreply.github.com>
2020-07-23 20:54:19 +0700

    Update wallpaper.py

--- Ilam/modules/wallpaper.py
+++ Ilam/modules/wallpaper.py
@@ -23,7 +23,7 @@ def wall(bot: Bot, update: Update, args):
     else:
         caption = query
         term = query.replace(" ", "%20")
-        json_rep = r.get(f"https://wall.alphacoders.com/api2.0/get.php?auth={4f1a9a2374e83bf681e4ab7c6d43fcd5}&method=search&term={term}").json()
+        json_rep = r.get(f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}").json()
         if not json_rep.get("success"):
             msg.reply_text(f"An error occurred! Report this {SUPPORT_CHAT}")
         else:
